8a8d20345e3457100618f0bac5adfab03e364164
Али Алиев <esedulleave@mail.ru>
2022-03-27 08:55:47 +0300

    site

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="content">
+    <Signup />
+  </div>
+</template>
+
+<script>
+import Media from './assets/css/media.css'
+import Signup from './components/Signup.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
+
+<style >
+
+</style>
